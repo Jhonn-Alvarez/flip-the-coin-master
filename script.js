27d14bc4a4ac45813052e61e967a63f12cd59b01
Jhonn-Alvarez <alvarezjhonn30@gmail.com>
2025-12-25 14:47:07 -0500

    created this flip a coin game that guesses if its Heads or tails. from DevChallenges

---
+++ script.js
@@ -0,0 +1,37 @@
+//This is where the logic will go
+
+const GameArea = document.getElementById('game-area');
+
+//Heads image
+const resultImage = document.createElement('img');
+GameArea.appendChild(resultImage);
+resultImage.src = '/resources/heads.svg';
+
+//shadow image
+const shadowImage = document.createElement('img');
+GameArea.appendChild(shadowImage);
+shadowImage.src = '/resources/shadow.svg';
+
+//result display
+const resultDisplay = document.createElement('p');
+resultDisplay.innerText = 'Heads';
+GameArea.appendChild(resultDisplay);
+
+//Button
+const flipButton = document.createElement('button');
+flipButton.innerText = 'Flip Coin';
+GameArea.appendChild(flipButton);
+
+//Flip function
+flipButton.addEventListener('click', function (e) {
+  const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
+  if (result == 'Heads') {
+    console.log('Heads');
+    resultImage.src = '/resources/heads.svg';
+    resultDisplay.innerText = result; 
+  } else {
+    console.log('Tails');
+    resultImage.src = '/resources/tails.svg';
+    resultDisplay.innerText = result;
+  }
+});
